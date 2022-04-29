ec39ddffa3b25a6d0d5eab8d9fb2c9344afe3b36
Dawn Olivier <dmarieolivier@gmail.com>
2022-04-29 14:55:22 -0400

    commit tree 899ae14105e73ccc2d5bf3df67ba56924f37b841

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(cb) {
+        return cb();
+    }
+function returnsANamedFunction() {
+    let namedFunction = "I am Dawn";
+    return function namedFunction(){}}
+
+function returnsAnAnonymousFunction() {
+    return function(){}}
